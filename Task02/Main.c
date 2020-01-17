25c81ab9408491041f06239b0dd1e7065c93662f
JEVLOGIN <jev.login@users.noreply.github.com>
2020-01-17 20:02:58 +0300

    Сделал второе задание, но без С

--- Task02/Main.c
+++ Task02/Main.c
@@ -32,6 +32,8 @@ int k;
 //	Подключили русский шрифт в консоли
 int i;
 
+int Ta = 1;
+
 void binPrint();
 
 void ConvertTwo(long N);
@@ -39,6 +41,12 @@ void ConvertTwo(long N);
 void convertToBin(long n);
 
 long convertToBinRecursion(long n);
+
+long PowerNotRecursion(int a, int b);
+
+long PowerRecursion(int a, int b);
+
+void Power(int a, int b);
 
 int main(int argc, char* argv[])
 {
@@ -52,12 +60,79 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Вы пропустили ввод значения.");
 		return 1;
 	}
-	//printf("Какой способ алгоритма выбираете:\n С рекурсией (1) или без нее (0)?'n Введите '1' или '0'\n");
 	printf("Какой способ алгоритма выбираете:\n");
 
 	ConvertTwo(N);
+	getch();
+	k = NULL;
+	system("cls");
+
+	printf("Задание 2.\n\n");
+	/*
+		2. Реализовать функцию возведения числа a в степень b:
+		a. без рекурсии;
+		b. рекурсивно;
+		c. *рекурсивно, используя свойство четности степени.
+	*/
+	int a, b;
+	
+	printf("Введите число и степень через пробел: ");
+	if (!(scanf("%i %i", &a, &b)))
+	{
+		fprintf(stderr, "Вы пропустили ввод значения.");
+		return 1;
+	}
+	printf("Какой способ алгоритма выбираете:\n");
+	
+	Power(a, b);
 
 	return 0;
+}
+
+void Power(int a, int b)
+{
+	do
+	{
+		printf("С рекурсией (1) или без нее (0)?'\n Введите '1' или '0'\n");
+	} while (!(scanf("%i", &k)) || k > 1);
+	if (k == 0)
+	{
+		system("cls");
+		PowerNotRecursion(a, b);
+	}
+	else if (k == 1)
+	{
+		system("cls");
+		PowerRecursion(a, b);
+	}
+	else
+	{
+		system("cls");
+		fprintf(stderr, "Вы ввели неверное значение!\n K может быть равно только или 1, или 0.\n Вы ввели %i\n", k);
+	}
+}
+
+long PowerRecursion(int a, int b)
+{
+	if (b)
+	{
+		Ta *= a;
+		b--;
+		return PowerRecursion(a, b);
+	}
+	return printf("Полученное Число %li", Ta);
+}
+
+long PowerNotRecursion(int a, int b)
+{
+	long p = 1;
+	int Tb = b;
+	while (Tb)
+	{
+		p *= a;
+		Tb--;
+	}
+	return printf("%i ^ %i = %li", a, b, p);
 }
 
 void ConvertTwo(long N)
